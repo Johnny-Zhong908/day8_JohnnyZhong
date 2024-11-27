c354aa75d4fb054bef41d33040bb98f8e72c1fc3
Johnny-Zhong908 <Zhongjo5@oocl.com>
2024-11-28 07:37:28 +0800

    Update MultipleCounter.jsx

--- src/MultipleCounter.jsx
+++ src/MultipleCounter.jsx
@@ -1,39 +1,37 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import CounterGroup from "./CounterGroup";
 
 const MultipleCounter = () => {
-    const initialSize = 0;
-    const [size, setSize] = useState(initialSize);
+  const initialSize = 0; 
+  const [size, setSize] = useState(initialSize);
 
-    const handleSizeChange = (event) => {
-        let newSize = parseInt(event.target.value, 10);
+  const handleSizeChange = (event) => {
+    let newSize = parseInt(event.target.value, 10);
+    if (newSize < 0) newSize = 0;
+    if (newSize > 20) newSize = 20;
+    setSize(newSize);
+  };
 
-        if (newSize < 0) newSize = 0;
-        if (newSize > 20) newSize = 20;
+  const handleReset = () => {
+    setSize(initialSize);
+  };
 
-        setSize(newSize);
-    };
-
-    const handleReset = () => {
-        setSize(initialSize);
-    };
-
-    return (
-        <div className="multiple-counter">
-            <div className="size-selector">
-                <label>Counter Size: </label>
-                <input
-                    type="number"
-                    value={size}
-                    onChange={handleSizeChange}
-                    min="0"
-                    max="20"
-                />
-                <button onClick={handleReset}>Reset</button>
-            </div>
-            <CounterGroup size={size} />
-        </div>
-    );
+  return (
+    <div className="multiple-counter">
+      <div className="size-selector">
+        <label>Counter Size: </label>
+        <input
+          type="number"
+          value={size}
+          onChange={handleSizeChange}
+          min="0"
+          max="20"
+        />
+        <button onClick={handleReset}>Reset</button>
+      </div>
+      <CounterGroup size={size} />
+    </div>
+  );
 };
 
 export default MultipleCounter;
