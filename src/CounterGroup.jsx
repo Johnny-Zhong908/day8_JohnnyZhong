90e9089148e82ff5ec2fcca2dc21bc4c688733d4
Johnny-Zhong908 <Zhongjo5@oocl.com>
2024-11-28 07:49:49 +0800

    Update CounterGroup.jsx

--- src/CounterGroup.jsx
+++ src/CounterGroup.jsx
@@ -1,46 +1,42 @@
-import Counter from "./Counter";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import Counter from './Counter';
 
 const CounterGroup = ({ size }) => {
-    const [counters, setCounters] = useState([]);
+  const [counters, setCounters] = useState([]);
 
-    useEffect(() => {
-        setCounters(Array(size).fill(0));
-    }, [size]);
+  useEffect(() => {
+    setCounters(Array(size).fill(0));
+  }, [size]);
 
-    const increment = (index) =>Counters = [...counters];
-        newCounters[index]++;
-        setCounters(newCounters);
-    };
+  const increment = (index) => {
+    const newCounters = [...counters];
+    newCounters[index]++;
+    setCounters(newCounters);
+  };
 
-    const decrement = (index) => {
-        const newCounters = [...counters];
-        newCounters[index]--;
-        setCounters(newCounters);
-    };
+  const decrement = (index) => {
+    const newCounters = [...counters];
+    newCounters[index]--;
+    setCounters(newCounters);
+  };
 
-    const reset = () => {
-        setCounters(Array(size).fill(0));
-    };
+  const sum = counters.reduce((acc, curr) => acc + curr, 0);
 
-    const sum = counters.reduce((acc, curr) => acc + curr, 0);
-
-    return (
-        <div className="counter-group">
-            {counters.map((value, index) => (
-                <Counter
-                    key={index}
-                    value={value}
-                    onIncrement={() => increment(index)}
-                    onDecrement={() => decrement(index)}
-                />
-            ))}
-            <div className="sum">
-                <p>Total Sum: {sum}</p>
-            </div>
-            <button onClick={reset}>Reset All</button>
-        </div>
-    );
+  return (
+    <div className="counter-group">
+      <div className="sum">
+        <p>Total Sum: {sum}</p>
+      </div>
+      {counters.map((value, index) => (
+        <Counter
+          key={index}
+          value={value}
+          onIncrement={() => increment(index)}
+          onDecrement={() => decrement(index)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CounterGroup;
